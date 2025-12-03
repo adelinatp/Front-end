e5760923f2ff7b2ec788e46b2ce7cd822af06faf
adelinatp <tapesadelina@gmail.com>
2025-12-03 15:22:28 +0200

    UI changes

--- API/script.js
+++ API/script.js
@@ -1,33 +1,20 @@
+
 fetch("https://dummyjson.com/products/")
     .then(response => response.json())
     .then(data => {
-        const tbody = document.querySelector("tbody")
+
         data.products.forEach(products => {
-            tbody.insertAdjacentHTML("beforebegin",
+            imagesContainer.insertAdjacentHTML("beforeend",
                 `
-                <tr>
-                    <td>${products.id} </td>
-                    <td>${products.title} </td>
-                    <td>${products.category} </td>
-                    <td>${products.price} </td>
-                    <td>${products.dimensions.width} </td>
-                    <td>${products.dimensions.height} </td>
-                    <td>${products.warrantyInformation} </td>
-                </tr>
+                <div class="card">
+                    <img src="${products.thumbnail}" alt="${products.title}"> </img>
+                    <h4>${products.title}</h4>
+                    <h6>${products.brand}</h6>
+                    <h6>${products.price} $</h6>
+                </div>
                 `
             )
-        
-            // const id =`<th>${products.id}</th>`
-            // const title =`<th>${products.title}</th>`
-            // const description =`<th>${products.description}</th>`
-            // const category =`<th>${products.category}</th>`
-            // const price =`<th>${products.price}</th>`
-            // document.querySelector(`tr`).insertAdjacentHTML("beforeend", title)
-            // document.querySelector(`tr`).insertAdjacentHTML("beforeend", description)
-            // document.querySelector(`tr`).insertAdjacentHTML("beforeend", category)
-            // document.querySelector(`tr`).insertAdjacentHTML("beforeend", price)
         })
     })
-
     .catch(error=>console.log(error))
     
